4f4935fea3f7d692f63aa52aedaf91d48c463455
CybertekSchool <github@cybertekschool.com>
2020-07-12 12:58:18 -0400

    added step defs

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -6,29 +6,39 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
+        String token;
+    Response response;
 
     @Given("I logged Bookit api using {string} and {string}")
     public void i_logged_Bookit_api_using_and(String email, String password) {
 
-        String token = BookItApiUtils.generateToken(email,password);
+        token = BookItApiUtils.generateToken(email,password);
         System.out.println("token = " + token);
 
     }
 
     @When("I get the current user information from api")
     public void i_get_the_current_user_information_from_api() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        String url = ConfigurationReader.get("qa1api.uri")+"/api/users/me";
+
+        response = given().header("Authorization", token)
+                .when().get(url);
+
+
+
     }
 
     @Then("status code should be {int}")
-    public void status_code_should_be(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+    public void status_code_should_be(int int1) {
+        Assert.assertEquals(response.statusCode(),int1);
+
+
     }
 
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -7,3 +7,7 @@ Feature: User Verification
     When I get the current user information from api
     Then status code should be 200
 
+  Scenario: verify information about logged user from api and database
+    Given I logged Bookit api using "sbirdbj@fc2.com" and "asenorval"
+    When I get the current user information from api
+    Then the information about current user from api and database should be match
