60b1915040fd5a3a367a90c7d5f2fb7f6123d84f
fkundysk <fkundysk@gmail.com>
2020-07-14 03:04:39 +0200

    update

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
-        String token;
+    String token;
     Response response;
     String emailGlobal;
 
--- src/test/java/com/bookit/utilities/ded.java
+++
@@ -1,4 +0,0 @@
-package com.bookit.utilities;
-
-public class ded {
-}
